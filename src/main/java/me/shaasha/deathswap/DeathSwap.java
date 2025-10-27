f31c65e59b3bde828e903a790b19d256ef5b9a29
Sasa571 <novaksimon01@gmail.com>
2025-10-27 12:45:18 +0100

    First full version of DeathSwap

--- src/main/java/me/shaasha/deathswap/DeathSwap.java
+++ src/main/java/me/shaasha/deathswap/DeathSwap.java
@@ -2,19 +2,16 @@ package me.shaasha.deathswap;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class DeathSwap extends JavaPlugin {
 
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new DeathCounter(), this);
         getServer().getPluginManager().registerEvents(new FreezeManager(), this);
-        getCommand("startdeathswap").setExecutor(new StartCommand());
-        getCommand("stopdeathswap").setExecutor(new StopCommand());
-    }
-
-    @Override
-    public void onDisable() {
-
+        Objects.requireNonNull(getCommand("startdeathswap")).setExecutor(new StartCommand());
+        Objects.requireNonNull(getCommand("stopdeathswap")).setExecutor(new StopCommand());
     }
 
     public static DeathSwap getInstance() {
--- src/main/java/me/shaasha/deathswap/StopCommand.java
+++ src/main/java/me/shaasha/deathswap/StopCommand.java
@@ -4,11 +4,12 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
 
 public class StopCommand implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         Bukkit.getScheduler().cancelTasks(DeathSwap.getInstance());
         sender.sendMessage("Deathswap stopped!");
         return true;
